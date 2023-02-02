b24118609367996ea1b871ea686b5edcd35a663d
xipper <864353360@qq.com>
2023-02-02 20:37:31 +0800

    switch

---
+++ golang/ch_07/switch/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	score := 72
+	var grade string
+	switch {
+	case score > 90:
+		grade = "A"
+	case score >= 80:
+		grade = "B"
+	case score >= 70:
+		grade = "C"
+	case score >= 60:
+		grade = "D"
+	case score >= 50:
+		grade = "E"
+	case score >= 40:
+		grade = "F"
+	default:
+		grade = "W"
+	}
+
+	fmt.Printf(grade)
+}
