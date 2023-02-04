16b75aca5efba72361e45b229e75525464cdaedd
xipper <864353360@qq.com>
2023-02-04 16:04:40 +0800

    array

--- golang/ch_08/array/main.go
+++ golang/ch_08/array/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	test1 := [...]string{"11"}
+	fmt.Println(test1)
+
+	test2 := [...]int{1: 1, 3: 2, 10: 3}
+	fmt.Println(test2)
+
+	for value, index := range test1 {
+		fmt.Println(value, index)
+	}
 }
